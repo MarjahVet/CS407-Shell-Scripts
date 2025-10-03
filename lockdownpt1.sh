7a94ea38e3f079b3d667c6860262f542544b2c12
MarjahVet <orcutte@SunyPoly.edu>
2025-10-03 16:04:09 -0400

    Add SSH key setup for admin user

--- lockdownpt1.sh
+++ lockdownpt1.sh
@@ -8,3 +8,8 @@ passwd admin
 
 mkdir -p /home/admin/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgP2fbcK91zpG9P1PaXFwhSbTjMHHaBG3+AKdYCgC4wG1fnhKDEZaZFnxLwv+t+GTMA/4mFdr7QK53tk5h2zlP6Qr3ERNFZ25zywGuWszTM50U9SH5UOw+M+Lv55Gpc0PvougXNXMKoYUwufiZAG+tZoYXaain3OCGzbFMMbas5YyICUavtwNyBQ7Wz0DmF14cWGvhP4p+r//kX/ybKol1SybR6VZmKkDtWLlI674bluQW1hubjqmYdILfVgvZSC+g3eSoqHvFfShQ7Xd5aoJBT4DGWv6kpl3/muLAKgi+ON3686KX2Un0erUIVl4dYSRCPSXUZe6uidJoJ/STFzZBYFjwsiTo4XOfIj/2A46TSFCeIYESN7f6kGNz+CB9V0B+v500HkTj7XUdgbMkGJEfBbm89/gjUDsI9Rq7zJI2wAnz6ritWOh+Awgj2ZOBUiNnUP4xxk8yIIZt+8aM+inPLXSCZP+uF9NZKN+SGOJVJikx38ecu5K42QtbLd1JNyE= naimols@fang" >> /home/admin/.ssh/authorized_keys
+
+chown -R admin:100 /home/admin
+chmod 700 /home/admin/.ssh
+chmod 600 /home/admin/.ssh/authorized_keys
+chmod 755 /home/admin
