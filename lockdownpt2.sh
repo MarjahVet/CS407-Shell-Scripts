ddf72c2f6942df72141c242a8776f9f19aea6241
MarjahVet <orcutte@SunyPoly.edu>
2025-10-03 16:03:34 -0400

    Remove permission settings for admin user
    Removed file permission changes for admin user.

--- lockdownpt2.sh
+++ lockdownpt2.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-chown -R admin:100 /home/admin
-chmod 700 /home/admin/.ssh
-chmod 600 /home/admin/.ssh/authorized_keys
-chmod 755 /home/admin
-
 vi /etc/ssh/sshd_config.d/01-permitrootlogin.conf
