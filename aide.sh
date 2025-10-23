c2a543c6788e0c14e45c64e3b3ce5b594e97d9e7
MarjahVet <orcutte@SunyPoly.edu>
2025-10-23 15:08:04 -0400

    Add AIDE installation and cron job setup
    Install AIDE, initialize database, and set up daily checks.

---
+++ aide.sh
@@ -0,0 +1,16 @@
+sudo dnf install -y aide
+sudo aide --init
+sudo cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+sudo aide --check
+
+sudo tee /etc/cron.d/aidecheck >/dev/null <<'End'
+# Run aide everyday @ 5:00 am
+0 5 * * * root /usr/sbin/aide --check > /var/log/aide/aidecheck.log 
+End
+
+sudo mkdir -p /var/log/aide
+sudo chmod 700 /var/log/aide
+sudo cat /etc/cron.d/aidecheck
+
+
+
