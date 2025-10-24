0dd0d55f808de4e72feaa4ff693f06bd57725c56
MarjahVet <orcutte@SunyPoly.edu>
2025-10-24 15:46:56 -0400

    Add VM backup script secBackup.sh
    This script allows users to back up specific virtual machines using rsync. It prompts for the VM name and performs the backup accordingly.

---
+++ secBackup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script for backing up VMs
+
+echo "test) Backup test"
+echo "www) Backup www"
+echo "auth) Backup auth"
+read -p "Please enter the name of the VM you wish to backup: " VM
+
+case $VM in
+	test)	  
+	  rsync -az -e ssh /etc admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /home admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  ssh admin@192.168.1.87
+	  ls /NEW_TANK/BACKUPS/$VM
+	  ;;
+	www)
+	  rsync -az -e ssh /etc admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /home admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /var/www admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  ;;
+	auth)
+	  rsync -az -e ssh /etc admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /home admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /var/named admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  rsync -az -e ssh /var/lib/ldap admin@192.168.1.87:/NEW_TANK/BACKUPS/$VM
+	  ;;
+	*)
+	  echo "Invalid option!"
+	  ;;
+esac
+	
